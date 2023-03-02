7dafb412db858748ffab43231a84ca5268e6eea9
Ilya Kaysin <demarkok@gmail.com>
2023-03-02 21:15:55 +0000

    Add normalization proofs

--- proofs/tex/auto/ord-and-norm-completeness.el
+++ proofs/tex/auto/ord-and-norm-completeness.el
@@ -1,6 +1,8 @@
 (TeX-add-style-hook
- "grammar"
+ "ord-and-norm-completeness"
  (lambda ()
+   (LaTeX-add-labels
+    "lemma:ord-completeness")
    (LaTeX-add-environments
     '("ottfundefnblock" LaTeX-env-args ["argument"] 2)
     '("ottdefnblock" LaTeX-env-args ["argument"] 2)))
